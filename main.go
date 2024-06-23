8f83a51b6d2c7491c3310db3979dfebe4c3b2e88
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2024-06-23 13:21:43 +0200

    Add json logs if it's executed in the background and add file logs to the service

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"flag"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/term"
 	"net/http"
+	"os"
 )
 
 func main() {
 	var port int
 	var configFile string
+	var logLevel string
+
 	flag.IntVar(&port, "port", 9081, "Port to listen on")
 	flag.StringVar(&configFile, "config", "./config.yaml", "Path to config file")
+	flag.StringVar(&logLevel, "loglevel", "info", "Log level")
 	flag.Parse()
 
 	c, err := getConfig(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = configureLogs(logLevel)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,4 +34,16 @@ func main() {
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), router); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
+}
+
+func configureLogs(logLevel string) error {
+	parsedLogLevel, err := log.ParseLevel(logLevel)
+	if err != nil {
+		return err
+	}
+	log.SetLevel(parsedLogLevel)
+	if !term.IsTerminal(int(os.Stdout.Fd())) {
+		log.SetFormatter(&log.JSONFormatter{})
+	}
+	return err
 }
